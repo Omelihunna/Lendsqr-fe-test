5023cfb77b3193bf16e7ab421d94453bc39bd6f6
“Omelihunna” <omelihunna@hotmail.com>
2025-07-03 15:25:26 +0100

    chore(global): SVG, title and routing updates for better page handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import LoginPage from "./pages/LoginPage.tsx";
 import DashboardLayout from "./layouts/DashboardLayout.tsx";
 import Users from "./pages/Users.tsx";
@@ -8,13 +8,14 @@ import UserDetailsPage from "./pages/UserDetailsPage.tsx";
 const App = () => {
     return (
         <Routes>
-            <Route path="/"></Route>
+            {/* <Route path=""></Route> */}
             <Route path="/login" element={<LoginPage/>}/>
             <Route path="/dashboard/*" element={<DashboardLayout />}>
                 <Route index element={<Users />} />
                 <Route index path="users" element={<Users />} />
                 <Route path="users/:id" element={<UserDetailsPage />} />
             </Route>
+            <Route path="*" element={<Navigate to="/login" replace />} />
         </Routes>
     )
 }
